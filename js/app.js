e098138f49093473f9b9d19a3b587e433ef23864
Administrator <admin@example.com>
2021-09-02 17:26:15 +0200

    7. Mostrar resultados del filtrado

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ const datosBusqueda = {
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos(); //Muestra los autos al cargar
+    mostrarAutos(autos); //Muestra los autos al cargar
 
     //Llenar las opciones de años
     llenarSelect()
@@ -68,7 +68,10 @@ color.addEventListener('change', e => {
 
 
 //Funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+
+    limpiarHTML(); //Elimina el HTML previo
+
     autos.forEach(auto => {
         
         const {marca, modelo, year, puertas, transmision, precio, color} = auto;
@@ -84,6 +87,14 @@ function mostrarAutos() {
     });
 }
 
+// Limpiar HTML
+function limpiarHTML() {
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+
 //Genera los años del select
 function llenarSelect(){
     for (let index = max; index >= min; index--) {
@@ -98,7 +109,8 @@ function llenarSelect(){
 function filtrarAuto() {
     const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
 
-    console.log(resultado);
+    // console.log(resultado);
+    mostrarAutos(resultado);
 }
 
 function filtrarMarca(auto) {
