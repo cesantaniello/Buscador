7db38576d1590df1a158114fd5b17ecd214a840f
Administrator <admin@example.com>
2021-09-03 09:57:52 +0200

    10. Mostrar mensaje cuando no hay resultados

--- js/app.js
+++ js/app.js
@@ -119,8 +119,21 @@ function llenarSelect(){
 function filtrarAuto() {
     const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor);
 
-    // console.log(resultado);
-    mostrarAutos(resultado);
+    if(resultado.length){
+        mostrarAutos(resultado);
+    }else{
+        noResultado();
+    }
+}
+
+function noResultado(){
+
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No hay resultados. Intenta con otros términos de búsqueda';
+    resultado.appendChild(noResultado)
 }
 
 function filtrarMarca(auto) {
